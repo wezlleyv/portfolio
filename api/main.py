f24abee9f28ede75b38a16a61ca166bc4f762a77
wezlleyv <weslleyonline72@outlook.com>
2023-04-12 18:11:01 -0300

    fixed bug

--- api/main.py
+++ api/main.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+if __name__ == "__main__":
+    app.run()
