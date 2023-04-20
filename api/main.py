70a2d7bdc72f639a0a02cec4e6f28a98b0a4ebd6
wezlleyv <weslleyonline72@outlook.com>
2023-04-20 19:28:47 -0300

    corrige all bugs and implent responsive mode

--- api/main.py
+++ api/main.py
@@ -3,13 +3,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+URL_LIST = {
+    'github': "https://github.com/wezlleyv",
+    'linkedin': "",
+    'instagram': "https://www.instagram.com/wezlleyvictor/",
+}
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", url=URL_LIST)
 
 @app.route("/projects")
 def projects():
-    return render_template('projects.html')
+    return render_template('projects.html', url=URL_LIST)
 
 if __name__ == "__main__":
     app.run()
